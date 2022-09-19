2ab41517e8b7cf3fce22b6cb462202346ac787e1
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-09-19 17:02:04 -0300

    Add html and css structure and javascript file

---
+++ index.js
@@ -0,0 +1,4 @@
+const bookList = document.querySelector('.book-list');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const addButton = document.querySelector('#addButton');
