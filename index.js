6c409df470d60bdf91b3389214646a38e036f932
Miliyonayalew <miliayalew@gmail.com>
2022-09-20 18:37:01 +0300

    Add javascript classes and methods to the project

--- index.js
+++ index.js
@@ -7,29 +7,38 @@ function Store(books) {
   localStorage.setItem('books', JSON.stringify(books));
 }
 
-let booksArray = JSON.parse(localStorage.getItem('books')) || [];
+const booksArray = JSON.parse(localStorage.getItem('books')) || [];
 
 const refreshList = () => {
   Store(booksArray);
   bookList.innerHTML = booksArray
     .map((data, index) => `<li><p>${data.title}</p><p>${data.author}</p>
-      <button onclick=removeBook(${index}) class="removeButton">Remove</button></li>`).join('');
+      <button onclick=Book.removeBooks(${index}) class="removeButton">Remove</button></li>`).join('');
 };
+
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  addBook() {
+    booksArray.push(this);
+  }
+
+  static removeBooks(index) {
+    booksArray.splice(index, 1);
+    refreshList();
+  }
+}
 
 refreshList();
 
-/* eslint-disable */
-const removeBook = (index) => {
-  booksArray = booksArray.filter((data, id) => id !== index);
-  refreshList();
-};
-
 addButton.addEventListener('click', (e) => {
   e.preventDefault();
-  const book = {
-    title: title.value,
-    author: author.value,
-  };
+  const book = new Book(title.value, author.value);
   booksArray.push(book);
   refreshList();
+  title.value = '';
+  author.value = '';
 });
